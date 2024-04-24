e9003d2b0275fcf68fd2cada59f1dd6128cf07b5
WingKill <zpswlsldi@gmail.com>
2024-04-24 16:46:52 +0900

    2 - Spring Security 암호화 객체는 해시 모듈이다.

--- spring_boot_ss_project/src/main/java/edu/sejong/ex/config/SecurityConfig.java
+++ spring_boot_ss_project/src/main/java/edu/sejong/ex/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package edu.sejong.ex.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.sejong.ex.security.CustomUserDetailsService;
 
@@ -17,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		// web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
@@ -27,8 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().antMatchers("/css/**", "/js/**", "/images/**", "/lib/**");
 	}
 
-	
-	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -37,36 +41,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		.withUser("member").password("{noop}member").roles("USER")
 //		.and()
 //		.withUser("admin").password("{noop}admin").roles("ADMIN");
-		
+
 		auth.userDetailsService(customUserDetailsService)
-			.passwordEncoder(new BCryptPasswordEncoder()); // 암호화 방식		
+			.passwordEncoder(passwordEncoder()); // 암호화 방식
 	}
-	
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// 우선 CSRF설정을 해제한다.
 		// 초기 개발시만 해주는게 좋다.
 		http.csrf().disable();
-		
+
 		// 권한 설정
-		http.authorizeRequests()
-		.antMatchers("/user/**").hasAnyRole("USER")
-		.antMatchers("/admin/**").hasAnyRole("ADMIN")
-		.antMatchers("/board/**").hasAnyRole("ADMIN")
-		//.antMatchers("/company/list").hasAnyRole("ADMIN")
-		.antMatchers("/**").permitAll();
-		
-		// 스프링 시큐리티가 기본적으로 가지고 있는 로그인 폼을 사용한다는 의미. 기본 설정이다. 
-		// http.formLogin(); 
-		http.formLogin()
-			.loginPage("/login")
-			//.loginPage("/elogin")
-			.usernameParameter("id")
-			.passwordParameter("pw")
-			//.defaultSuccessUrl("/board/list2")
-			//.defaultSuccessUrl("/index")
-			.defaultSuccessUrl("/")
-			.permitAll(); // 모든 사람에게 허용하겠다는 의미.
+		http.authorizeRequests().antMatchers("/user/**").hasAnyRole("USER").antMatchers("/admin/**").hasAnyRole("ADMIN")
+				.antMatchers("/board/**").hasAnyRole("ADMIN")
+				// .antMatchers("/company/list").hasAnyRole("ADMIN")
+				.antMatchers("/**").permitAll();
+
+		// 스프링 시큐리티가 기본적으로 가지고 있는 로그인 폼을 사용한다는 의미. 기본 설정이다.
+		// http.formLogin();
+		http.formLogin().loginPage("/login")
+				// .loginPage("/elogin")
+				.usernameParameter("id").passwordParameter("pw")
+				// .defaultSuccessUrl("/board/list2")
+				// .defaultSuccessUrl("/index")
+				.defaultSuccessUrl("/").permitAll(); // 모든 사람에게 허용하겠다는 의미.
 	}
 }
--- spring_boot_ss_project/src/test/java/edu/sejong/ex/mapper/UserMapperTest.java
+++ spring_boot_ss_project/src/test/java/edu/sejong/ex/mapper/UserMapperTest.java
@@ -1,11 +1,14 @@
 package edu.sejong.ex.mapper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.sejong.ex.vo.AuthVO;
 import edu.sejong.ex.vo.UserVO;
@@ -17,24 +20,27 @@ class UserMapperTest {
 
 	@Autowired
 	private UserMapper userMapper;
-	
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Test
 	void test() {
 		assertNotNull(userMapper);
 	}
-	
+
 	@Test
 	void testSelectUserAuthLists() {
 		UserVO user = userMapper.selectUserAuths("admin3");
-		log.info("유저정보 : "+user);
-		for(AuthVO auth : user.getAuthorities()) {
-			log.info("권한정보 : "+auth);
-		}	
+		log.info("유저정보 : " + user);
+		for (AuthVO auth : user.getAuthorities()) {
+			log.info("권한정보 : " + auth);
+		}
 	}
 
 	@Test
 	void testInsertUser() {
-		
+
 //		UserVO user = new UserVO();
 //		user.setUserName("admin2");
 //		user.setPassword(new BCryptPasswordEncoder().encode("admin2"));
@@ -42,24 +48,29 @@ class UserMapperTest {
 //		
 //		userMapper.insertUser(user);
 //		userMapper.insertAuthority(user);
-		
+
+//		UserVO user = new UserVO();
+//		user.setUserName("admin3");
+//		user.setPassword(new BCryptPasswordEncoder().encode("admin3"));
+//		user.setEnabled("1");
+//		
+//		userMapper.insertUser(user);
+//		userMapper.insertAuthorityAdmin(user);
+
 		UserVO user = new UserVO();
-		user.setUserName("admin3");
-		user.setPassword(new BCryptPasswordEncoder().encode("admin3"));
+		user.setUserName("user2");
+		user.setPassword(passwordEncoder.encode("user2"));
 		user.setEnabled("1");
-		
+
 		userMapper.insertUser(user);
-		userMapper.insertAuthorityAdmin(user);
+		userMapper.insertAuthority(user);
 	}
-	
-//	@Test
-//	void testInsertAuthorities() {
-//		List<AuthVO> auths = new ArrayList<AuthVO>();
-//		AuthVO authVo = new AuthVO("hello","ROLE_USER");
-//		auths.add(authVo);
-//		UserVO user = new UserVO("hello","hihello");
-//		user.setAuthorities(auths);
-//		userMapper.insertAuthority(user);		
-//		log.info("유저정보 : "+userMapper.selectUserAuths("hello"));
-//	}
+
+	@Test
+	void testMatcher() {
+		UserVO user = userMapper.selectUserAuths("admin2");
+		boolean isMatch = passwordEncoder.matches("admin2", user.getPassword());
+		assertEquals(isMatch, true);
+	}
+
 }
